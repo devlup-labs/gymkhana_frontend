b6ce4c2b9c6f5d86fb32f03ac308ec6ee83a580e
ajatprabha <ajat.prabha.leo@gmail.com>
2019-05-24 17:45:57 +0530

    fix eslint errors

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,18 @@
     </v-toolbar>
 
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
