64437d9e3ea0a3993d37dadaa7ae1f6f5e0d822e
killbotxd <sonawane.1@iitj.ac.in>
2019-05-27 12:43:06 +0530

    added a background image to the login page

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-app></v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style>
+#app {
+  background-image: url("../assets/workstation-336369.jpg");
+  background-size: cover;
+  overflow: hidden;
+}
+</style>
