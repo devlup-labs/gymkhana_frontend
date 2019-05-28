90657ee4243952f204bdea6665af91679dc36f54
killbotxd <sonawane.1@iitj.ac.in>
2019-05-28 14:32:28 +0530

    google sign in fix with requested changes

---
+++ src/components/LoginCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-toolbar class="pt-2 blue darken-4">
+      <v-toolbar-title class="white--text "><h4>Login</h4></v-toolbar-title>
+    </v-toolbar>
+    <v-card>
+      <v-card-text class="pt-4">
+        <div>
+          <v-form v-model="valid" ref="form">
+            <v-text-field
+              outline
+              prepend-icon="mdi-email "
+              label="Username"
+              v-model="email"
+              :rules="emailRules"
+              required
+            ></v-text-field>
+            <!--mdi-visibility not working fix it-->
+            <v-text-field
+              outline
+              prepend-icon="mdi-lock"
+              :append-icon="
+                showPassword ? 'mdi-visibility' : 'mdi-visibility_off'
+              "
+              :append-icon-cb="() => (showPassword = !showPassword)"
+              :rules="passwordRules"
+              :type="showPassword ? 'password' : 'text'"
+              label="Password"
+              min="8"
+              required
+              v-model="password"
+            ></v-text-field>
+            <v-layout justify-center>
+              <v-btn color="pink" class="white--text">Login</v-btn>
+            </v-layout>
+            <v-layout justify-center>
+              <p>OR</p>
+            </v-layout>
+            <v-layout justify-center>
+              <v-btn outline color="white">
+                <img src="../assets/btn_google_light_normal.svg" height="90" />
+              </v-btn>
+            </v-layout>
+          </v-form>
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginCard"
+};
+</script>
+
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,59 +6,9 @@
         <v-layout flex align-center offset-xs5 fill-height justify-center>
           <v-flex xs11 sm6 elevation-6>
             <!--Header-->
-            <v-toolbar class="pt-2 blue darken-4">
-              <v-toolbar-title class="white--text "
-                ><h4>Login</h4></v-toolbar-title
-              >
-            </v-toolbar>
+
             <!--Header-->
-            <!--Body-->
-            <v-card>
-              <v-card-text class="pt-4">
-                <div>
-                  <v-form v-model="valid" ref="form">
-                    <v-text-field
-                      outline
-                      prepend-icon="mdi-email "
-                      label="Username"
-                      v-model="email"
-                      :rules="emailRules"
-                      required
-                    ></v-text-field>
-                    <!--mdi-visibility not working fix it-->
-                    <v-text-field
-                      outline
-                      prepend-icon="mdi-lock"
-                      :append-icon="
-                        showPassword ? 'mdi-visibility' : 'mdi-visibility_off'
-                      "
-                      :append-icon-cb="() => (showPassword = !showPassword)"
-                      :rules="passwordRules"
-                      :type="showPassword ? 'password' : 'text'"
-                      label="Password"
-                      min="8"
-                      required
-                      v-model="password"
-                    ></v-text-field>
-                    <v-layout justify-center>
-                      <v-btn color="pink" class="white--text">Login</v-btn>
-                    </v-layout>
-                    <v-layout justify-center>
-                      <p>OR</p>
-                    </v-layout>
-                    <v-layout justify-center>
-                      <v-btn outline color="white">
-                        <img
-                          src="../assets/btn_google_light_normal.svg"
-                          height="90"
-                        />
-                      </v-btn>
-                    </v-layout>
-                  </v-form>
-                </div>
-              </v-card-text>
-            </v-card>
-            <!--Body-->
+            <LoginCard />
           </v-flex>
         </v-layout>
       </v-container>
@@ -67,6 +17,7 @@
 </template>
 
 <script>
+import LoginCard from "../components/LoginCard.vue";
 export default {
   name: "Login",
   data() {
@@ -78,6 +29,9 @@ export default {
       email: "",
       emailRules: [v => !!v || "E-mail is required"]
     };
+  },
+  components: {
+    LoginCard
   }
 };
 </script>
