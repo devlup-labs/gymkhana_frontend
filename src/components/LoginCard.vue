c534d55c7a04f71f79e03f8950280b8fa2a1ab0c
killbotXD <42870378+killbotXD@users.noreply.github.com>
2019-05-29 14:03:29 +0530

    basic login with G-sign in (#3)
    * created login page
    
    * added a background image to the login page
    
    * added a login toolbar layout
    
    * created login card
    
    * added outline to the textviews
    
    * google sign in fix
    
    * final with all requested changes

---
+++ src/components/LoginCard.vue
@@ -0,0 +1,67 @@
+<!--suppress CheckImageSize -->
+<template>
+  <div>
+    <v-toolbar class="pt-2 blue darken-4">
+      <v-toolbar-title class="white--text "><h4>Login</h4></v-toolbar-title>
+    </v-toolbar>
+    <v-card>
+      <v-card-text class="pt-4">
+        <div>
+          <v-form v-model="valid" ref="form">
+            <v-text-field
+              outline
+              prepend-icon="mdi-email "
+              label="Username"
+              v-model="email"
+              :rules="emailRules"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :append-icon-cb="() => (showPassword = !showPassword)"
+              :rules="passwordRules"
+              :type="showPassword ? 'password' : 'text'"
+              label="Password"
+              min="8"
+              required
+              v-model="password"
+            ></v-text-field>
+            <v-layout justify-center>
+              <v-btn color="pink" class="white--text">Login</v-btn>
+            </v-layout>
+            <v-layout justify-center>
+              <p>OR</p>
+            </v-layout>
+            <v-layout justify-center>
+              <v-btn outline color="white">
+                <img :src="googleSignInBtn" height="90" alt="sign in google" />
+              </v-btn>
+            </v-layout>
+          </v-form>
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import googleSignInBtn from "../assets/btn_google_light_normal.svg";
+export default {
+  name: "LoginCard",
+  data() {
+    return {
+      valid: false,
+      showPassword: false,
+      password: "",
+      passwordRules: [v => !!v || "Password is required"],
+      email: "",
+      emailRules: [v => !!v || "E-mail is required"],
+      googleSignInBtn
+    };
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid fill-heignt class="loginOverlay">
+    <v-layout flex justify-start></v-layout>
+    <v-layout flex align-center offset-xs5 fill-height justify-center>
+      <v-flex xs11 sm6 elevation-6>
+        <LoginCard />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import LoginCard from "../components/LoginCard.vue";
+export default {
+  name: "Login",
+  components: {
+    LoginCard
+  }
+};
+</script>
+
+<style></style>
