6f20fa63da7d4ee214b1a277d54f299ebf0b263a
killbotxd <sonawane.1@iitj.ac.in>
2019-06-04 14:52:57 +0530

    wrote tests for login component

---
+++ tests/unit/login.spec.js
@@ -0,0 +1,21 @@
+import { mount } from "@vue/test-utils";
+import LoginCard from "@/components/LoginCard.vue";
+
+describe("Component", () => {
+  const wrapper = mount(LoginCard);
+  const textfield = wrapper.findAll("v-text-field");
+  test("Password changes visibility on clicking the append icon", () => {
+    expect(wrapper.vm.showPassword).toBe(false);
+    textfield.at(1).trigger("click:append");
+    expect(wrapper.vm.showPassword).toBe(true);
+  });
+  test("On clicking the preped icon visibility should change", () => {
+    expect(wrapper.html()).toContain("v-text-field");
+
+    // textfield.find('append-icon').trigger('click')
+    //expect(wrapper.vm.showPassword).toBe(true)
+  });
+  test("webpage must contain google sign in button", () => {
+    expect(wrapper.find("img.gsign-responsive").exists()).toBe(true);
+  });
+});
