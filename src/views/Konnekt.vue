eda913c0bd5f5060e86d8b2fb1ea56d730c0c9c4
killbotxd <sonawane.1@iitj.ac.in>
2019-07-02 17:16:19 +0530

    created konnekt searchpage

--- src/views/Konnekt.vue
+++ src/views/Konnekt.vue
@@ -1,31 +1,16 @@
 <template>
-  <div class="konnekt-background">
-    <v-container>
-      <v-layout row justify-center mt-5 pt-4 fill-height>
-        <v-flex lg6 offset-xs3
-          ><v-text-field
-            outline
-            prepend-inner-icon="mdi-magnify"
-            label="search any skill or name"
-            clearable
-          ></v-text-field
-        ></v-flex>
-        <v-flex><v-btn color="primary">GO</v-btn></v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+  <home />
 </template>
 
 <script>
+import home from "../components/KonnektHome";
+
 export default {
-  name: "Konnekt"
+  name: "Konnekt",
+  components: {
+    home
+  }
 };
 </script>
 
-<style scoped>
-.konnekt-background {
-  height: 100%;
-  background: url("../assets/konnekt/konnekt.svg") no-repeat center;
-  background-size: cover;
-}
-</style>
+<style scoped></style>
