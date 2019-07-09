7278fabbcbf4da58c3526317989fba28e4d173f3
killbotxd <sonawane.1@iitj.ac.in>
2019-07-09 14:46:14 +0530

    resolved the $vuetify issue

--- src/components/KonnektSearch.vue
+++ src/components/KonnektSearch.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   div.container
     v-container
-      v-responsive( min-width="40px")
-        v-layout( row fill-height)
-          v-flex( lg6 offset-xs3)
+      v-responsive(min-width="40px")
+        v-layout(row fill-height)
+          v-flex(lg6 offset-xs3)
             v-text-field(
               v-model="search"
               outline
@@ -11,12 +11,12 @@
               label="search any skill or name"
               clearable)
           v-flex
-            v-btn(large="" color="primary") GO
+            v-btn(large color="primary") GO
     v-container(v-responsive__sizer v-responsive__content)
       div(v-for="person in filteredList" v-bind:key="person.name")
         v-container.container
           v-layout.card( row wrap flex-center)
-            v-flex( xs3 lg2 md2 pa-3 layout justify-center)
+            v-flex(xs3 lg2 md2 pa-3 layout justify-center)
               v-avatar.elevation-4(size="100" :tile="false")
                 v-img(v-bind:src="person.img" alt="profile image")
             v-flex(
@@ -28,10 +28,7 @@
               xs4
               lg1
               md1
-              :class={
-                'pt-4 mt-0': $vuetify.breakpoint.smAndDown,
-                'pt-0 mt-4': $vuetify.breakpoint.mdAndUp
-              }
+              :class="{ 'pt-4 mt-0': $vuetify.breakpoint.smAndDown, 'pt-0 mt-4': $vuetify.breakpoint.mdAndUp }"
             )
               h2.mb-3.font-weight-light {{ person.name }}
             v-flex(
@@ -40,20 +37,14 @@
               xs5
               lg8
               md8
-              :class={
-                'pt-3 mt-0': $vuetify.breakpoint.smAndDown,
-                'pt-1 mt-4': $vuetify.breakpoint.mdAndUp
-              }
+              :class="{ 'pt-3 mt-0': $vuetify.breakpoint.smAndDown, 'pt-1 mt-4': $vuetify.breakpoint.mdAndUp }"
             )
-              v-chip(
-                elevation-2
-                font-weight-bold
+              v-chip.elevation-2.font-weight-bold(
                 xs2
                 color="light-blue darken-1 white--text"
                 v-for="skill in person.skills"
                 v-bind:key="skill.name"
               ) {{ skill }}
-
 </template>
 
 <script>
