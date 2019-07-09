d77f2c8c7cbfdc6f9732c34528421e5a66f641fb
killbotXD <42870378+killbotXD@users.noreply.github.com>
2019-07-09 15:34:14 +0530

    Konnekt using pug (#10)
    * changed KonnektHome
    
    * changed KonnektSearch
    
    * resolved the $vuetify issue

--- src/components/KonnektHome.vue
+++ src/components/KonnektHome.vue
@@ -1,19 +1,15 @@
-<template>
-  <div class="konnekt-background">
-    <v-container>
-      <v-layout row justify-center mt-3 pt-4 fill-height>
-        <v-flex lg6 offset-xs3
-          ><v-text-field
+<template lang="pug">
+  div.konnekt-background
+    v-container
+      v-layout(row justify-center mt-3 pt-4 fill-height)
+        v-flex(lg6 offset-xs3)
+          v-text-field(
             outline
             prepend-inner-icon="mdi-magnify"
             label="search any skill or name"
-            clearable
-          ></v-text-field
-        ></v-flex>
-        <v-flex><v-btn large color="primary">GO</v-btn></v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+            clearable)
+        v-flex
+          v-btn(large color="primary") GO
 </template>
 
 <script>
