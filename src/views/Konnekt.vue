bec9d323cca6d99da2b9c53b639fb3808b225c0b
killbotxd <sonawane.1@iitj.ac.in>
2019-07-12 17:34:23 +0530

    added topbar with navigation drawer

--- src/views/Konnekt.vue
+++ src/views/Konnekt.vue
@@ -1,13 +1,17 @@
-<template>
-  <Home />
+<template lang="pug">
+  div
+    Toolbar :title="this.$options.name" />
+    Home
 </template>
 
 <script>
 import Home from "../components/KonnektHome";
+import Toolbar from "../components/Toolbar";
 
 export default {
   name: "Konnekt",
   components: {
+    Toolbar,
     Home
   }
 };
