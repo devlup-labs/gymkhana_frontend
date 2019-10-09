615e75f739b4ca6212117ac77c70af2eed31151c
killbotXD <sonawane.1@iitj.ac.in>
2019-10-09 17:48:47 +0530

    add Header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
-<template>
-  <v-app>
-    <v-content>
-      <router-view />
-    </v-content>
-  </v-app>
+<template lang="pug">
+  v-app
+    Header
+    v-content
+      v-container(fluid fill-height)
+        router-view
 </template>
 
 <script>
+import Header from "./components/common/Header";
+
 export default {
   name: "App",
+  components: {
+    Header
+  },
   data() {
     return {
       //
---
+++ src/components/common/Header.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div
+    v-app-bar.px-md-12(dark dense)
+      img.mr-4(:src="logo" height="40")
+
+      v-toolbar-items
+        v-btn(text) Home
+        v-btn(text) Societies
+        v-btn(text) Forum
+        v-btn(text) People
+      v-spacer
+</template>
+
+<script>
+import GymkhanaLogo from "../../assets/logo.png";
+export default {
+  name: "Header",
+  computed: {
+    logo: () => GymkhanaLogo
+  }
+};
+</script>
+
+<style scoped></style>
