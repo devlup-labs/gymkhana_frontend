d74b5d9e3cddca30e9f4107179dc6ca3052b715f
killbotxd <sonawane.1@iitj.ac.in>
2019-10-09 18:10:52 +0530

    create office bearer view

---
+++ src/views/OfficeBearers.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div
+    Toolbar(:title="this.$options.name")
+    v-card.text-lg-center.text-xs-center.text-md-center(class="blue text-center").ma-5
+     v-card-text(class="white--text")
+      h1.font-weight-regular Office Bearers
+    v-container(fluid fill-height)
+      v-layout(flex align-center offset-md5 fill-height justify-center)
+        v-flex( xs10 sm8 lg6)
+          OfficeBearerCard
+</template>
+
+<script>
+import Toolbar from "../components/Toolbar";
+import OfficeBearerCard from "../components/OfficeBearerCard";
+export default {
+  name: "OfficeBearers",
+  components: {
+    Toolbar,
+    OfficeBearerCard
+  }
+};
+</script>
+
+<style scoped></style>
