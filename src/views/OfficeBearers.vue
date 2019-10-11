5109bf9652d27f2d3698b234449e10f4f8d9ea51
killbotxd <sonawane.1@iitj.ac.in>
2019-10-11 16:37:48 +0530

    change text size in office bearer header and fixed width

--- src/views/OfficeBearers.vue
+++ src/views/OfficeBearers.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   v-content.justify-center(justify-center)
-    v-card.text-lg-center.text-xs-center.text-md-center(class="blue text-center")
+    v-card.text-center(class="blue text-center mx-auto" max-width="800").mt-4
       v-card-text(class="white--text")
-        h2.font-weight-regular Office Bearer
+        h1.font-weight-regular Office Bearer
     v-container
       v-row
         v-col(cols="12" md="4" v-for="x in 3")
