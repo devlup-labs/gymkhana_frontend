6ab3e887551558e2084bbae99c327e89a07061ba
killbotxd <sonawane.1@iitj.ac.in>
2019-11-14 04:43:55 +0530

    fix card height

--- src/components/OfficeBearerCard.vue
+++ src/components/OfficeBearerCard.vue
@@ -2,7 +2,7 @@
   v-stepper(v-model="cardPage").pa-0
     v-stepper-items
       v-stepper-content.pa-0(step="1")
-        v-card
+        v-card(:height="avatarSize*3")
           v-img(src="https://students.iitj.ac.in/static/assets/others/cover2.svg" :height="avatarSize")
           v-card-text.pt-0.text-center
             v-avatar.elevation-4.ma-2(:size="avatarSize" :style="{'margin-top': `-${avatarSize/2}px !important`}")
@@ -15,11 +15,11 @@
                 v-icon mdi-refresh
                 v-text Details
       v-stepper-content.pa-0(step="2")
-        v-card(height="400px")
+        v-card(:height="avatarSize*3")
           v-row
             v-col
               v-row
-                v-card-text.justify-center.text-md-center.text-xs-center.text-lg-center
+                v-card-text.justify-center.text-center
                   h2.font-weight-medium.mb-5  About Anugrah
               v-row
                 v-divider
@@ -28,13 +28,13 @@
                   a(href="https://students.iitj.ac.in/office-bearers/tel:9610942435") +91-9610942435
               v-row.justify-center
                 div
-                  v-row.justify-center
+                  v-row
                     v-icon  mdi-email
                     v-text.ma-2.pa-2 kumar.14@iitj.ac.in
-              v-row
+              v-row.justify-center.align-end.pa-0.ma-0.fh
                 v-divider
-              v-row.justify-center.fill-height.align-end.pa-0.ma-0
-                v-btn(@click="cardPage = 1" depressed)
+              v-row.justify-center.mt-2.pa-0
+                v-btn(@click="cardPage = 1" text)
                   v-icon mdi-refresh
                   v-text Back
 
@@ -54,4 +54,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.fh {
+  height: 50%;
+}
+</style>
