4a71312c0abe3007a5c438f85be4f11b939b3d5a
killbotxd <sonawane.1@iitj.ac.in>
2019-12-13 23:59:25 +0530

    fix header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
   v-app-bar.px-md-12(app dark dense)
     img.mr-4(:src="logo" height="40")
-      v-toolbar-items
-        v-btn(text) Home
-        v-btn(text) Societies
-        v-btn(text) Forum
-        v-btn(text href="../office-bearers") People
-      v-spacer
+
+    v-toolbar-items
+    v-btn(text) Home
+    v-btn(text) Societies
+    v-btn(text) Forum
+    v-btn(text href="../office-bearers") People
+    v-spacer
 </template>
 
 <script>
