c6473408910056dc82d6f0539e29cc740eef9b77
killbotxd <sonawane.1@iitj.ac.in>
2019-12-26 00:17:24 +0530

    create basic Society.vue

--- src/views/Society.vue
+++ src/views/Society.vue
@@ -1,12 +1,36 @@
 <template lang="pug">
-  div
-    p hello
+  v-content.pt-0
+    v-parallax(src="../assets/home1.jpg" cover).society-photo.text-center.align-center
+      v-overlay(absolute)
+        p(class="white--text").display-2 Society of Design and Arts
+    v-container
+      v-layout(row).ma-4
+        v-flex.md8
+          v-card(flat tile text)
+            v-card-title.headline About
+        v-flex.md4
+          v-card(flat tile text)
+            v-card-title.headline.justify-center
+              v-icon(left) mdi-pen
+              | Upcoming Event
+            v-card-text
+              
+    Footer
 </template>
 
 <script>
+import Footer from "../components/common/Footer";
 export default {
-  name: "Society"
+  name: "Society",
+  components: { Footer }
 };
 </script>
 
-<style scoped />
+<style scoped>
+.society-photo {
+  height: 20rem !important;
+  background: url("../assets/home1.jpg") no-repeat center !important;
+  background-size: cover !important;
+  -webkit-background-size: cover !important;
+}
+</style>
