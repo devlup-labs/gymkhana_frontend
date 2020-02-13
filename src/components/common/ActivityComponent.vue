29bed3bf37862eab0b870642cee65046d7ea9e6e
killbotxd <sonawane.1@iitj.ac.in>
2020-02-14 04:31:51 +0530

    create activity component

---
+++ src/components/common/ActivityComponent.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  v-expansion-panels(accordion focusable)
+    v-expansion-panel(v-for='({node},i) in activitiesData.edges' :key='i')
+      v-expansion-panel-header {{node.name}}
+      v-expansion-panel-content.mt-2
+        | {{node.description.substring(3,node.description.length-4)}}
+</template>
+
+<script>
+export default {
+  name: "ActivityComponent",
+  props: {
+    activitiesData: {}
+  }
+};
+</script>
+
+<style scoped></style>
