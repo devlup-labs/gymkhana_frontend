a74964e9a6a1fad4f6e9a2cbf6e387041607a72e
killbotXD <sonawane.1@iitj.ac.in>
2020-03-06 04:56:24 +0530

    create festivalCarousel component

---
+++ src/components/FestivalCarousel.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  v-carousel( v-if="festivals"  hide-delimiter-background show-arrows-on-hover).text-center
+    v-carousel-item(
+      transition="fade-transition"
+      reverse-transition="fade-transition"
+      v-for="({node}, i) in festivals.edges" :key="i"
+    )
+      v-row.justify-center
+        div.display-1 {{node.name.toUpperCase()}}
+        v-btn(icon ripple large :to="node.slug" right)
+          v-icon mdi-link
+      v-row.mt-6
+        v-col
+          v-row.justify-center
+            v-img( :src="node.photo.sizes[2].url" max-height="350" max-width="450")
+        v-col
+          v-row.font-weight-regular.mt-2.justify-lg-start.justify-center
+            v-col(cols="10")
+              p.subtitle-2.text-center.font-weight-bold  {{node.about.substring(3,node.about.length-4)}}
+</template>
+
+<script>
+export default {
+  name: "FestivalCarousel",
+  props: {
+    festivals: null
+  }
+};
+</script>
+
+<style scoped></style>
