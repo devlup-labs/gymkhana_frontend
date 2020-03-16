d8d39309d23887855d58272ba73e6e6809bf7add
killbotXD <sonawane.1@iitj.ac.in>
2020-03-16 15:52:44 +0530

    create core member component

---
+++ src/components/common/CoreMemberComponent.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  v-layout(row).justify-center.ma-2
+    v-flex.md6
+      v-card.elevation-6
+        v-img(:src="memberData.avatar" max-height="200")
+    v-flex.md6
+      v-layout(column).align-center.ma-4
+        v-flex.mt-4 {{memberData.user.firstName}}
+        v-flex.mt-4 {{memberData.phone}}
+        //v-flex.pa-4
+          v-layout(row)
+            v-btn(icon v-for="(sl,l) in volunteer.socialLinks" :key="l" :href="sl.link")
+              v-icon.pa-2 {{sl.icon}}
+</template>
+
+<script>
+export default {
+  name: "coreMemberComponent",
+  props: {
+    memberData: null
+  }
+};
+</script>
+
+<style scoped></style>
