48b24ae6010bfbdece61e194b7958e181e4cd72b
killbotXD <sonawane.1@iitj.ac.in>
2020-03-20 21:37:53 +0530

    update home query for event and news table

--- src/graphql/queries/homeSocietyQuery.js
+++ src/graphql/queries/homeSocietyQuery.js
@@ -1,5 +1,7 @@
 import gql from "graphql-tag";
 import { SIZES_FRAGMENT } from "../fragments/sizesFregment";
+import { NEWS_FRAGMENT } from "../fragments/newsFragment";
+import { EVENT_FRAGMENT } from "../fragments/eventFragment";
 
 export const GET_SOCIETIES_QUERY = gql`
   query {
@@ -13,10 +15,20 @@ export const GET_SOCIETIES_QUERY = gql`
             cover {
               ...sizesFields
             }
+            clubSet {
+              edges {
+                node {
+                  ...eventFields
+                  ...newsFields
+                }
+              }
+            }
           }
         }
       }
     }
   }
   ${SIZES_FRAGMENT}
+  ${NEWS_FRAGMENT}
+  ${EVENT_FRAGMENT}
 `;
