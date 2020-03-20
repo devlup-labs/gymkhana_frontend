238da18e7f931fbca85a7f11766adb6ff6c0fb30
killbotXD <sonawane.1@iitj.ac.in>
2020-03-20 22:27:31 +0530

    populate office bearers

--- src/views/OfficeBearers.vue
+++ src/views/OfficeBearers.vue
@@ -2,19 +2,36 @@
   v-content.justify-center(justify-center)
     v-card.text-center(class="blue text-center mx-auto" max-width="800").mt-4
       v-card-text(class="white--text")
-        h1.font-weight-regular Office Bearer
-    v-container
+        h1.font-weight-regular Office Bearers
+    v-container(v-for="({node},i) in societies.edges.flatMap(e=>e) " :key="i")
+      v-card-title.display-1.justify-center {{node.name}}
       v-row
-        v-col(cols="12" md="4" v-for="x in 3" :key="x")
-          OfficeBearerCard(:avatarSize="120")
+        v-col(cols="12" md="4")
+          OfficeBearerCard(:avatarSize="120" :bearerData="node.mentor")
+        v-col(cols="12" md="4")
+          OfficeBearerCard(:avatarSize="120" :bearerData="node.secretary")
+        v-col(cols="12" md="4")
+          OfficeBearerCard(:avatarSize="120" :bearerData="node.jointSecretary")
 </template>
 
 <script>
 import OfficeBearerCard from "../components/OfficeBearerCard";
+import { GET_OFFICE_BEARERS_QUERY } from "../graphql/queries/officeBearersQuery";
 export default {
+  apollo: {
+    societies: {
+      query: GET_OFFICE_BEARERS_QUERY
+    }
+  },
   name: "OfficeBearers",
   components: {
     OfficeBearerCard
+  },
+  mounted() {
+    console.log("lodssg");
+    var data = [];
+    this.societies.edges.flatMap(e => data.push(e));
+    console.log(data);
   }
 };
 </script>
