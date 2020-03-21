eac49a269b218d8307481d1b64a613558574bf53
killbotXD <sonawane.1@iitj.ac.in>
2020-03-21 20:25:59 +0530

    refactor FestivalCarousel.vue

--- src/components/FestivalCarousel.vue
+++ src/components/FestivalCarousel.vue
@@ -3,20 +3,24 @@
     v-carousel-item(
       transition="fade-transition"
       reverse-transition="fade-transition"
-      v-for="({node}, i) in festivalsList" :key="i"
+      v-for="({ node }, i) in festivalsList" :key="i"
     )
       v-row.justify-center
-        div.display-1 {{node.name.toUpperCase()}}
+        div.display-1 {{ node.name.toUpperCase() }}
         v-btn(icon ripple large :to="node.slug" right)
           v-icon mdi-link
       v-row.mt-6
         v-col
           v-row.justify-center
-            v-img( :src="node.photo.sizes.find(e => e.name === 'full_size').url" max-height="350" max-width="450")
+            v-img(
+              :src="node.photo.sizes.find(e => e.name === 'full_size').url"
+              max-height="350"
+              max-width="450"
+            )
         v-col
           v-row.font-weight-regular.mt-2.justify-lg-start.justify-center
             v-col(cols="10")
-              p.subtitle-2.text-center.font-weight-bold  {{node.about.substring(3,node.about.length-4)}}
+              p.subtitle-2.text-center.font-weight-bold(v-html="node.about")
 </template>
 
 <script>
