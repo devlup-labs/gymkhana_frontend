56c53af6311e0a9b88eab8ab5abdcc242b832877
killbotXD <sonawane.1@iitj.ac.in>
2020-03-22 21:30:41 +0530

    Create custom Ligth gallery component

---
+++ src/components/common/CustomLightGallery.vue
@@ -0,0 +1,88 @@
+<template lang="pug">
+  v-row
+    v-col(v-for="({node},i) in images" cols="12"  sm="8" md="4").offset-md-0.offset-sm-2
+      v-tooltip(bottom)
+        template(v-slot:activator="{on}")
+          v-hover(v-slot:default="{ hover }")
+            v-card.mx-auto(
+              :elevation="hover ? 24 : 0"
+              @click.stop="showLG(node)"
+            )
+              v-img(
+                :src="node.image.sizes[0].url"
+                height="250px"
+                v-on="on"
+              )
+        span {{node.title}}
+    v-overlay(:value="dialog" opacity=".95")
+      v-container
+        v-row.justify-end
+          v-btn(@click.stop="hideLG()" icon)
+            v-icon mdi-close
+        v-container
+          v-carousel(
+            :height="carouselHeight"
+            hide-delimiters
+            show-arrows
+            v-model="lgIndex"
+          )
+            v-carousel-item(
+              transition="fade-transition"
+              reverse-transition="fade-transition"
+              v-for="({node}, i) in images"
+              :key="i"
+            )
+              v-row.justify-center.fill-height.align-center
+                v-col(cols="10" )
+                  v-row.justify-center.mb-1
+                    v-card.font-weight-light.headline.pa-2 {{node.title}}
+                  v-row
+                    v-img(:src="node.image.sizes[0].url")
+</template>
+
+<script>
+export default {
+  name: "CustomLightGallery",
+  props: {
+    images: { Type: Array, required: true }
+  },
+  data: () => ({
+    dialog: false,
+    lgIndex: 0
+  }),
+  methods: {
+    showLG(node) {
+      this.lgIndex = this.images.flatMap(({ node }) => node).indexOf(node);
+      this.dialog = !this.dialog;
+    },
+    hideLG() {
+      this.dialog = !this.dialog;
+    }
+  },
+  computed: {
+    carouselHeight() {
+      return window.innerHeight / 1.3;
+    }
+  }
+};
+</script>
+
+<style scoped>
+@media (max-width: 960px) {
+  .container {
+    max-width: 750px;
+  }
+}
+
+@media (max-width: 750px) {
+  .container {
+    max-width: 550px;
+  }
+}
+
+@media (max-width: 550px) {
+  .container {
+    max-width: 400px;
+  }
+}
+</style>
