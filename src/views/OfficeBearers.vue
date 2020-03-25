ce3145784788e1396419b0c18f5fd88a4b853a6c
killbotXD <sonawane.1@iitj.ac.in>
2020-03-25 23:59:05 +0530

    add nullckeck on office bearers

--- src/views/OfficeBearers.vue
+++ src/views/OfficeBearers.vue
@@ -3,20 +3,21 @@
     v-card.text-center(class="blue text-center mx-auto" max-width="800").mt-4
       v-card-text(class="white--text")
         h1.font-weight-regular Office Bearers
-    v-container(v-for="({ node }, i) in societies.edges" :key="i")
+    v-container(v-for="({ node }, i) in societies.edges" :key="i" v-if=" node.mentor || node.secretary || node.jointSecretary ")
       v-card-title.display-1.justify-center {{ node.name }}
-      v-row.justify-space-around
-        v-col(cols="12" md="4" sm="6")
+      v-row.justify-space-around(v-if=" node.mentor || node.secretary || node.jointSecretary ")
+        v-col(cols="12" md="4" sm="6" v-if="node.mentor")
           OfficeBearerCard(:avatarSize="120" :profile="node.mentor" :designation="'Mentor'")
-        v-col(cols="12" md="4" sm="6")
+        v-col(cols="12" md="4" sm="6" v-if="node.secretary")
           OfficeBearerCard(:avatarSize="120" :profile="node.secretary" :designation="'Secretary'")
-        v-col(cols="12" md="4" sm="6")
+        v-col(cols="12" md="4" sm="6" v-if="node.jointSecretary")
           OfficeBearerCard(:avatarSize="120" :profile="node.jointSecretary" :designation="'Joint Secretary'")
 </template>
 
 <script>
 import OfficeBearerCard from "../components/OfficeBearerCard";
 import { GET_OFFICE_BEARERS_QUERY } from "../graphql/queries/officeBearersQuery";
+
 export default {
   apollo: {
     societies: {
