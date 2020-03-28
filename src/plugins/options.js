50effff3d540ef3035472242f8c5c83b4d1697ea
killbotXD <sonawane.1@iitj.ac.in>
2020-03-28 20:00:43 +0530

    add colors to sociel media icon  map

--- src/plugins/options.js
+++ src/plugins/options.js
@@ -1,11 +1,11 @@
 export default {
   socialMediaIconMap: {
-    FB: { name: "Facebook", icon: "mdi-facebook" },
-    TW: { name: "Twitter", icon: "mdi-twitter" },
-    LI: { name: "LinkedIn", icon: "mdi-linkedin" },
-    GP: { name: "Google Plus", icon: "mdi-google-plus" },
-    IG: { name: "Instagram", icon: "mdi-instagram" },
-    GH: { name: "GitHub", icon: "mdi-github-circle" },
-    YT: { name: "YouTube", icon: "mdi-youtube" }
+    FB: { name: "Facebook", icon: "mdi-facebook", color: "#3b5998" },
+    TW: { name: "Twitter", icon: "mdi-twitter", color: "#00acee" },
+    LI: { name: "LinkedIn", icon: "mdi-linkedin", color: "#0e76a8" },
+    GP: { name: "Google Plus", icon: "mdi-google-plus", color: "#db4a39" },
+    IG: { name: "Instagram", icon: "mdi-instagram", color: "#E1306C" },
+    GH: { name: "GitHub", icon: "mdi-github-circle", color: "#211F1F" },
+    YT: { name: "YouTube", icon: "mdi-youtube", color: "#c4302b" }
   }
 };
