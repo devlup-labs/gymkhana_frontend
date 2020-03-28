94336c8072b162da9689fbc7ceed68216a22b3cd
killbotXD <sonawane.1@iitj.ac.in>
2020-03-28 20:10:23 +0530

    Create query for viewer profile

---
+++ src/graphql/queries/viewerProfileQuery.js
@@ -0,0 +1,39 @@
+import gql from "graphql-tag";
+import { SIZES_FRAGMENT } from "../fragments/sizesFragment";
+import { FULL_NAME_FRAGMENT } from "../fragments/userFullNameFragment";
+import { SOCIAL_MEDIA_ICON_FRAGMENT } from "../fragments/socialMediaIconFragment";
+
+export const VIEWER_PROFILE_QUERY = gql`
+  query viewer {
+    viewer {
+      username
+      ...NameParts
+      email
+      userprofile {
+        socialLinks {
+          edges {
+            node {
+              ...SocialMedia
+            }
+          }
+        }
+        roll
+        gender
+        phone
+        dob
+        prog
+        year
+        branch
+        hometown
+        cover {
+          ...Sizes
+        }
+        skills
+        about
+      }
+    }
+  }
+  ${SIZES_FRAGMENT}
+  ${FULL_NAME_FRAGMENT}
+  ${SOCIAL_MEDIA_ICON_FRAGMENT}
+`;
