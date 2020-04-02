809ead5f500e7ef95b7ba8d785bb1a219a719f56
killbotXD <sonawane.1@iitj.ac.in>
2020-04-03 04:28:43 +0530

    add logout method and populate viewer data

--- src/components/common/SidenavHeader.vue
+++ src/components/common/SidenavHeader.vue
@@ -12,15 +12,18 @@
               span(v-if="$vuetify.breakpoint.mdAndUp") {{ item.title }}
               v-icon(v-if="$vuetify.breakpoint.mdAndUp && item.children") mdi-menu-down
           v-list(v-if="item.children")
-            v-list-item(v-for="(child, i) in item.children" :key="i" :to="child.to")
-              v-btn(text block) {{ child.title }}
+            v-list-item
+              v-btn(text block :to='{name : "profile"}' link) Account
+            v-list-item
+              v-btn(text block @click="logout1") Logout
     v-navigation-drawer(v-model="drawer" app :clipped="$vuetify.breakpoint.lgAndUp" :mini-variant.sync="mini")
       template(v-slot:prepend)
         v-list-item(two-line v-if="!$apollo.queries.viewer.loading")
           v-list-item-avatar
-            img(src="https://randomuser.me/api/portraits/women/81.jpg")
-          v-list-item-content
-            v-list-item-title {{ viewer.firstName }} {{ viewer.lastName }}
+            img(:src="viewer.userprofile.avatar.sizes.find(e => e.name === 'medium_square_crop').url")
+          v-list-item-content.text-start.pt-4
+            v-list-item-title {{ viewer.firstName }}
+            v-list-item-title {{ viewer.lastName }}
             v-list-item-subtitle {{ viewer.email }}
           v-list-item-action
             v-btn(icon @click.stop="mini = !mini")
@@ -40,14 +43,15 @@
 </template>
 
 <script>
-import { VIEWER_PROFILE_QUERY } from "../../graphql/queries/viewerProfileQuery";
+import { onLogout } from "../../plugins/vue-apollo";
+import { GET_SIDENAV_DATA_QUERY } from "../../graphql/queries/sidenavDataQuery";
 
 export default {
   name: "SidenavHeader",
   props: { title: { type: String } },
   apollo: {
     viewer: {
-      query: VIEWER_PROFILE_QUERY
+      query: GET_SIDENAV_DATA_QUERY
     },
     $client: "private"
   },
@@ -78,7 +82,14 @@ export default {
       { title: "Help", icon: "mdi-help" },
       { title: "About", icon: "mdi-information" }
     ]
-  })
+  }),
+  methods: {
+    logout1() {
+      onLogout(this.$apollo.provider.clients.private).then(() => {
+        this.$router.push({ name: "login" });
+      });
+    }
+  }
 };
 </script>
 
