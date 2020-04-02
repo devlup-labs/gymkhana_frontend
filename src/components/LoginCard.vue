3d6b7f7225d0ca4ffa30109006c009398549fe92
killbotXD <sonawane.1@iitj.ac.in>
2020-04-03 04:27:40 +0530

    create login methond and fix icon logic

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -6,7 +6,7 @@
     v-card.tile
       v-card-text(pt-4)
         div
-          v-form(v-model="valid" ref="form")
+          v-form(v-model="valid" ref="form" @submit.prevent="login1")
             v-text-field(
               outline
               prepend-icon="mdi-email"
@@ -16,20 +16,20 @@
               required
             )
 
-            v-text-field(v-toolbar
-              outline
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            v-text-field(
+            outline
+            prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
               @click:append="() => (showPassword = !showPassword)"
               :rules="passwordRules"
-              :type="showPassword ? 'password' : 'text'"
+              :type="showPassword ? 'text' : 'password'"
               label="Password"
               min="8"
               required
               v-model="password"
             )
             v-layout(justify-center)
-              v-btn(color="primary") Login
+              v-btn(color="primary" type="submit") Login
             v-layout(justify-center)
               h4 OR
             v-layout(justify-center)
@@ -43,6 +43,9 @@
 </template>
 <script>
 import googleSignInBtn from "../assets/btn_google_light_normal.svg";
+import { onLogin } from "../plugins/vue-apollo";
+import { AUTHORIZATION_MUTATION } from "../graphql/mutations/authMutation";
+
 export default {
   name: "LoginCard",
   data() {
@@ -55,6 +58,31 @@ export default {
       emailRules: [v => !!v || "E-mail is required"],
       googleSignInBtn
     };
+  },
+  methods: {
+    login1() {
+      this.$apollo
+        .mutate({
+          mutation: AUTHORIZATION_MUTATION,
+          // Parameters
+          variables: {
+            username: this.email,
+            password: this.password
+          },
+          client: "private"
+        })
+        .then(data => {
+          onLogin(
+            this.$apollo.provider.clients.private,
+            data.data.tokenAuth.token
+          );
+          this.$router.push({ name: "konnekt-home" });
+        })
+        .catch(error => {
+          // Error
+          alert(error.message);
+        });
+    }
   }
 };
 </script>
