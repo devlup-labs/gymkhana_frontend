15120ac1063783f3b9747f1ffd88cb715fbddb5c
killbotXD <sonawane.1@iitj.ac.in>
2020-04-03 04:30:37 +0530

    add avatar and cover prop in profile card and populate data

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
   v-card(:min-height="getCardHeight").elevation-4
-    v-img(src="https://students.iitj.ac.in/static/assets/others/cover2.svg" :height="getCardHeight/2")
+    v-img(v-if="coverLink" :src="coverLink" :height="getCardHeight/2")
+    v-img(v-else src="https://students.iitj.ac.in/static/assets/others/cover2.svg" :height="getCardHeight/2")
     v-col.justify-center.text-center
       v-flex
         v-avatar.elevation-4.justify-center.ma-2(
           :size="getCardHeight/2"
           :style="{'margin-top': `-${getCardHeight/4}px !important`}")
-          v-img.justify-center(src="https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png")
+          v-img.justify-center(v-if="avatarLink" :src="avatarLink")
+          v-img.justify-center(v-else src="https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png")
       v-flex
         h2(class="black--text").font-weight-bold.mt-0 {{ name }}
       v-flex
@@ -19,7 +21,9 @@ export default {
   props: {
     cardHeight: Number,
     name: String,
-    rollNumber: String
+    rollNumber: String,
+    avatarLink: String,
+    coverLink: String
   },
   computed: {
     getCardHeight() {
