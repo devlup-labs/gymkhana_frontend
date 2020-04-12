81240a7f3195927a6e5f01ad16f0b88716075ad6
killbotXD <sonawane.1@iitj.ac.in>
2020-04-12 23:45:07 +0530

    Create dialog component for adding answers

---
+++ src/components/AddAnswerDialog.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-card
+    v-card-title.pl-6.pt-6
+      p Topic Name
+      v-spacer
+      slot(name="cross")
+    v-divider
+    v-card-subtitle
+      p.pt-4 Answer
+      v-textarea(label="Description" v-model="description" :counter="300" outlined).mt-2
+    v-card-actions.mr-6.pb-6.justify-end
+      slot(name="answer")
+</template>
+
+<script>
+export default {
+  name: "AddAnswerDialog",
+  data: () => ({
+    description: null
+  })
+};
+</script>
+
+<style scoped></style>
