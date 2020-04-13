1dedfbb72af44b7bfe2416d84292cba208ea7a79
killbotXD <sonawane.1@iitj.ac.in>
2020-04-14 04:07:51 +0530

    populate and pass data from apollo query

--- src/components/forum/ForumTopic.vue
+++ src/components/forum/ForumTopic.vue
@@ -1,30 +1,42 @@
 <template lang="pug">
-  v-container(fluid).pt-0
+  v-container(fluid v-if="!$apollo.queries._topic.loading").pt-0
     v-row.text-capitalize.justify-center.align-center.fill-height(class="blue white--text").display-2.pa-8
-      |Topic
+      | {{topic.title}}
     v-container
       v-row.mt-3
         v-avatar(size="100")
-          v-img(src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcST4n1T70ibu7ov-7FT63MjRA-yPrjrfHem04kPtqOVWjBNQuQK")
+          v-img(:src="topic.author.avatar?topic.author.avatar.sizes.find(e=>e.name==='full_size').url:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcST4n1T70ibu7ov-7FT63MjRA-yPrjrfHem04kPtqOVWjBNQuQK'")
         v-col.pl-4
           v-row
-            p Soham Sonawane
-            p.ml-2.font-weight-light April 8, 2020, 2:23 a.m.
+            p {{topic.author.user.firstName.concat(' ',topic.author.user.lastName)}}
+            p.ml-2.font-weight-light {{createdAt}}
             v-row.justify-end.mr-8.align-center
-              UpvoteButton.justify-end.mr-4
+              UpvoteButton.justify-end.mr-4(:upvotes="topic.upvotesCount" :upvoted="topic.isUpvoted" )
               v-btn(icon @click="toggleDialog")
                 v-icon mdi-reply
-          span Description
-          v-row
+          span(v-if="topic.content" v-html="topic.content")
+          v-row(v-if="topic.tags")
             v-chip.elevation-2.font-weight-bold.ma-1(
               xs2
             color="light-blue darken-1 white--text"
-            v-for="i in 3"
+            v-for="(tag,i) in topic.tags.split(',')"
             :key="i"
-          ) tags
+          ) {{tag}}
       v-divider.mt-5
-      v-timeline(align-top dense)
-        ForumAnswerComponent(v-for="t in 4" :key="t")
+      v-timeline(align-top dense v-if="topic.answerSet.edges.length")
+        ForumAnswerComponent(v-for="({node},t) in topic.answerSet.edges" :key="t"
+        :authorAvatar="node.author.avatar.sizes.length?node.author.avatar.sizes.find(e=>e.name==='full_size').url:null"
+        :authorName="node.author.user.firstName.concat(' ',node.author.user.lastName)"
+        :answerTime="timeSince(node.createdAt)"
+        :answerContent="node.content"
+        :isUpvoted="node.isUpvoted"
+        :upvotes="node.upvotesCount"
+        :isAuthor="node.isAuthor"
+        )
+      v-row(v-else).justify-center.display-1.font-weight-light.ma-6
+        v-btn(icon @click="toggleDialog")
+          v-icon mdi-reply
+        |Be the first to answer
       v-dialog(v-model="dialog" persistent max-width="700px" )
         AddAnswerDialog
           template(v-slot:cross)
@@ -38,7 +50,21 @@
 import ForumAnswerComponent from "./ForumAnswerComponent";
 import UpvoteButton from "../common/buttons/UpvoteButton";
 import AddAnswerDialog from "./AddAnswerDialog";
+import { GET_TOPIC_QUERY } from "../../graphql/queries/topicQuery";
+import moment from "moment";
 export default {
+  apollo: {
+    _topic: {
+      query: GET_TOPIC_QUERY,
+      variables() {
+        return {
+          slug: this.$route.params.slug
+        };
+      },
+      update: data => data.topic
+    },
+    $client: "private"
+  },
   name: "ForumTopic",
   components: { AddAnswerDialog, UpvoteButton, ForumAnswerComponent },
   data: () => ({
@@ -47,6 +73,17 @@ export default {
   methods: {
     toggleDialog() {
       this.dialog = !this.dialog;
+    },
+    timeSince(date) {
+      return moment(date, "YYYYMMDDLTS").fromNow();
+    }
+  },
+  computed: {
+    topic() {
+      return this._topic.edges[0].node;
+    },
+    createdAt() {
+      return moment(this.topic.createdAt).format("MMMM DD, YYYY, hh:mm a");
     }
   }
 };
