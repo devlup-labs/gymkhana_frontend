fd97dfc6f6f256b4ea5935158ffa1252044f8b25
killbotXD <sonawane.1@iitj.ac.in>
2020-04-14 01:38:11 +0530

    populate pagination data in forum home

--- src/components/forum/ForumHome.vue
+++ src/components/forum/ForumHome.vue
@@ -14,11 +14,19 @@
               v-btn(color="primary" ).mb-2
                 v-icon mdi-magnify
           v-divider.mb-5
-          v-row.justify-center
-            v-col(cols="12" md="10" xs="12" v-for="x in 3" :key="x")
-              ForumTopicCard
+          v-row.justify-center(v-if="search")
+            v-col(cols="12" md="10" xs="12" v-for="({node},x) in this.search.edges" :key="x")
+              ForumTopicCard(:topic="node.title" :topicId="node.id" :authorName="authorName(node)" :authorInfo="authorInfo(node)" :answerAuthorName="answerAuthorName(node)"
+                :answerTime="node.answerSet.edges.length  ?timeSince(node.answerSet.edges[0].node.createdAt):null"
+                :authorPic="node.author.avatar.sizes.find(e=>e.name=='full_size').url")
+                template(v-slot:upVote)
+                  UpvoteButton.justify-lg-center.pl-10(:upvotes="node.upvotesCount" :upvoted="node.isUpvoted").justify-sm-end
+                template(v-slot:answersCount)
+                  CommentsCounter(:answerCount="node.answersCount")
+                template(v-slot:deleteButton)
+                  TopicDeleteButton(v-if="node.isAuthor")
     v-container.mt-5
-        v-pagination(circle :length="10" v-model="page" total-visible="7" next-icon="mdi-chevron-right" prev-icon="mdi-chevron-left")
+        v-pagination(circle :length="pages" v-model="page" total-visible="7" next-icon="mdi-chevron-right" prev-icon="mdi-chevron-left")
     v-dialog(v-model="dialog" persistent max-width="700px" )
       AddTopicDialog
         template(v-slot:cross)
@@ -46,10 +54,41 @@
 <script>
 import ForumTopicCard from "./ForumTopicCard";
 import AddTopicDialog from "./AddTopicDialog";
+import { GET_FORUM_TOPICS_QUERY } from "../../graphql/queries/forumTopicsQuery";
+import UpvoteButton from "../common/buttons/UpvoteButton";
+import CommentsCounter from "../common/buttons/CommentsCounter";
+import TopicDeleteButton from "../common/buttons/TopicDeleteButton";
 
 export default {
+  apollo: {
+    search: {
+      query: GET_FORUM_TOPICS_QUERY,
+      variables() {
+        if (this.searchTerm) {
+          return {
+            query: this.searchTerm,
+            first: this.page * 3,
+            last: 3
+          };
+        } else {
+          return {
+            query: "",
+            first: this.page * 3,
+            last: 3
+          };
+        }
+      }
+    },
+    $client: "private"
+  },
   name: "Forum",
-  components: { AddTopicDialog, ForumTopicCard },
+  components: {
+    TopicDeleteButton,
+    CommentsCounter,
+    UpvoteButton,
+    AddTopicDialog,
+    ForumTopicCard
+  },
   data: () => ({
     searchTerm: null,
     page: 1,
@@ -58,6 +97,59 @@ export default {
   methods: {
     toggleDialog() {
       this.dialog = !this.dialog;
+      console.log(this.search.edgeCount);
+      console.log(this.pages);
+    },
+    authorName(node) {
+      return node.author.user.firstName.concat(" ", node.author.user.lastName);
+    },
+    authorInfo(node) {
+      return node.author.prog.concat(
+        ", ",
+        node.author.branch,
+        ", ",
+        node.author.year
+      );
+    },
+    answerAuthorName(node) {
+      if (node.answerSet.edges.length)
+        return this.authorName(node.answerSet.edges[0].node);
+      else return null;
+    },
+    timeSince(date) {
+      date = new Date(date).getTime();
+      var seconds = Math.floor((new Date() - date) / 1000);
+      var interval = Math.floor(seconds / 31536000);
+
+      if (interval > 1) {
+        return interval + " years";
+      }
+      interval = Math.floor(seconds / 2592000);
+      if (interval > 1) {
+        return interval + " months";
+      }
+      interval = Math.floor(seconds / 86400);
+      if (interval > 1) {
+        return interval + " days";
+      }
+      interval = Math.floor(seconds / 3600);
+      if (interval > 1) {
+        return interval + " hours";
+      }
+      interval = Math.floor(seconds / 60);
+      if (interval > 1) {
+        return interval + " minutes";
+      }
+      return Math.floor(seconds) + " seconds";
+    }
+  },
+  computed: {
+    pages() {
+      console.log(this.page);
+      if (this.searchTerm && this.search)
+        return Math.ceil(this.search.edgeCount / 3);
+      else if (this.search) return Math.ceil(this.search.totalCount / 3);
+      return 1;
     }
   }
 };
